d3e349ed334de64aad242507f61b1e35acddb459
Michal Gutowski <michal.gutowski2002@gmail.com>
2020-10-05 11:13:34 +0200

    wypelnianie planszy testowym sudoku

--- script.js
+++ script.js
@@ -29,3 +29,68 @@ function makeGame() {
 	}
 	pole.innerHTML = wstaw;
 }
+let sudokuPlanszaTest = [1,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,1,3,4,5,6,7,8,9,1,2,4,5,6,7,8,9,1,2,3,5,6,7,8,9,1,2,3,4,6,7,8,9,1,2,3,4,5,7,8,9,1,2,3,4,5,6,8,9,1,2,3,4,5,6,7,9,1,2,3,4,5,6,7,8];
+function fillGame(plansza){
+	let id1 = 1;
+	let id2 = 1;
+		for(var i=0;i<=8;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=9;i<=17;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=18;i<=26;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=27;i<=35;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=36;i<=44;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=45;i<=53;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=54;i<=62;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=63;i<=71;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+		id1++;
+		id2 = 1;			
+		for(var i=72;i<=80;i++){
+			handleInput(id1,id2,plansza[i])
+			id2++;
+		}
+}
+function handleInput(id1,id2,value){
+	//debug
+	console.log("Ustawiam wartosc "+value+" dla id="+id1+'_'+id2)
+	if(value != 0){
+		document.getElementById(id1+'_'+id2).value = value;
+		document.getElementById(id1+'_'+id2).disabled = true;
+	}
+}
--- walidacja.js
+++ walidacja.js
@@ -22,18 +22,3 @@ function validateGame() {
         } else { suma = 0; }
     }
 }
-//debug
-function fillbyid() {
-    for (var a = 1; a <= 9; a++) {
-        for (var b = 1; b <= 9; b++) {
-            document.getElementById(a + '_' + b).value = b;
-        }
-    }
-}
-function fillbyrow() {
-    let offset = 0;
-    let numbers = [1, 2, 3, 3, 4, 5, 5, 6, 7]
-    for (var i in numbers) {
-        document.getElementById(1 + '_' + i).value = numbers[i];
-    }
-}
